c5efda7e4fb9c4dea38dcfd8fd7557f064504d4d
root <root@outernet.(none)>
2016-02-06 23:06:15 -0500

    Commiting changes from migration to CentOS VPS

---
+++ service.wsgi
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '/var/www/html/voltulator')
+
+from voltulator_app import app as application
--- voltulator_app.py
+++ voltulator_app.py
@@ -1,15 +1,15 @@
+#! python
 import os
 import datetime
-from voltulator import Voltulator, PriceGrabber
+from voltulator import Voltulator
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'D:\TestUpload'
+UPLOAD_FOLDER = '/tmp/'
 ALLOWED_EXTENSIONS = set(['csv'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -18,6 +18,7 @@ def allowed_file(filename):
 def upload_file():
     if request.method == 'POST':
         file = request.files['infile']
+        cents = request.form['incents']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -25,10 +26,9 @@ def upload_file():
             now = datetime.datetime.now()
             monthNum = now.strftime("%m")
 
-            priceGrab = PriceGrabber('http://rates.northwesternenergy.com/residentialelectricrates.aspx')
-            price = priceGrab.grab()
+            price = float(cents)/100 
 
-            calc = Voltulator(os.path.join(app.config['UPLOAD_FOLDER'], filename), price, monthNum)
+            calc = Voltulator(os.path.join(app.config['UPLOAD_FOLDER'], filename), price, None)
             output = calc.modifyCSV()
 
             return render_template('table.html', table=output)
